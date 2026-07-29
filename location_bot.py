19f0cad076b46b3631f31e1cd2acd898ccb8f8a5
watifer <watifer@gmail.com>
2026-07-29 10:44:48 +0200

    Initial commit

--- location_bot.py
+++ location_bot.py
@@ -250,7 +250,7 @@ def main_bot():
                 bot_username=BOT_USERNAME,  # Pobiera Twoją zmienną BOT_USERNAME z konfiguracji pliku!
                 get_coords_fn=get_coords_from_city,
                 # Adaptery lambda dopasowujące Twoje istniejące funkcje do silnika gościa:
-                build_payload_fn=lambda lat, lon, lang, is_now: build_payload_for_location(lat, lon, lang=lang, is_now=True),
+                build_payload_fn=lambda lat, lon, lang, is_now: build_payload_for_location(lat, lon, lang=lang),
                 prepare_layout_fn=lambda payload: prepare_now_layout_data(payload),
                 render_png_fn=image_generator.generate_weather_card,
                 send_photo_fn=lambda c_id, path: send_photo(
